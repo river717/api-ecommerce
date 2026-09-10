a2fababc0a5a070ac8e7314a46aa7a6882cb0ff8
Marvin Rivera <marvinrivera@dizucar.com>
2026-09-10 13:29:21 -0600

    Se agregaron las migraciones de base de datos

---
+++ database/migrations/2026_09_10_191801_create_payments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('order_id')
+                ->constrained('orders')
+                ->cascadeOnDelete();
+
+            $table->string('stripe_payment_intent_id')
+                ->unique();
+
+            $table->decimal('amount', 10, 2);
+
+            $table->string('currency', 3)
+                ->default('usd');
+
+            $table->enum('status', [
+                'pending',
+                'succeeded',
+                'failed',
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
